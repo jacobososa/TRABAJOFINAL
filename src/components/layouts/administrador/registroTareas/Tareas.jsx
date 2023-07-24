5473967348e47532569d2cd8f5a7018fea6a5984
207 <207@academicos.cesde.edu.co>
2023-07-24 15:25:55 -0500

    basedatos administrador

--- src/components/layouts/administrador/registroTareas/Tareas.jsx
+++ src/components/layouts/administrador/registroTareas/Tareas.jsx
@@ -1,13 +1,50 @@
+import { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { database } from "../../clientes/config/Tareaconnection"
 import HeaderAdmin from "../../../helpers/HeaderAdmin"
 
 
 const Tareas = () => {
-  
+  const [listaTareas, setListaTareas] = useState([]);
+  const mostrarTareas = async () => {
+    const tareasCollection = collection(database, "Tarea-Administrador");
+    const data = await getDocs(tareasCollection);
+    setListaTareas(data.docs.map((doc) => ({ ...doc.data() })));
+    console.log(data.docs.map((doc) => ({ ...doc.data() })));
+  };
+  useEffect(() => {
+    mostrarTareas();
+  }, []);
   return (
-    <>
-        <HeaderAdmin/>
-       
-    </>
+    <section>
+  <HeaderAdmin/>
+  <h3 className="titutarea">Tareas</h3>
+      <section className="table-container">
+        <table className="creative-table">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Documento</th>
+              <th>Tarea a Realizar</th>
+              <th>Descripción de la Tarea</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listaTareas.map((tareaItem) => (
+              <tr className="uno"  key={tareaItem.nombre}>
+                <td>{tareaItem.nombre}</td>
+                <td>{tareaItem.documento}</td>
+                <td>{tareaItem.tarea}</td>
+                <td>
+                  {tareaItem.descripción}
+                </td>
+              </tr>
+            ))
+            }
+          </tbody>
+        </table>
+      </section>   
+    </section>
   )
 }
 
