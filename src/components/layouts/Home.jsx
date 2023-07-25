0bf93b182f9739e84a5035b9acaac87d8822b7c6
207 <207@academicos.cesde.edu.co>
2023-07-25 12:18:13 -0500

    avances

--- src/components/layouts/Home.jsx
+++ src/components/layouts/Home.jsx
@@ -1,30 +1,54 @@
-import HeaderCliente from '../helpers/HeaderCliente'
-import HeaderAdmin from '../helpers/HeaderAdmin'
-import { Link } from 'react-router-dom';
-
+import HeaderCliente from "../helpers/HeaderCliente";
+import HeaderAdmin from "../helpers/HeaderAdmin";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
-
+  const [PinValid, setPinValid] = useState(false);
+  const [inputPin, setInputPin] = useState("");
+  const PinValidacion = () => {
+    if (inputPin == "2222") {
+      setPinValid(true);
+    } else {
+      alert("El pin es incorrecto");
+    }
+  };
+  if (PinValid) {
+    window.location.href = "/paginaadministrador";
+    return null;
+  }
+  
   return (
-    <main>
-      <HeaderAdmin />
+    <section>
+    <HeaderAdmin />
       <HeaderCliente />
-    <section className="contenedor__todo">
-      <section class="caja__trasera">
-        <section class="caja__trasera-login">
-          <h3>¿Eres un administrador?</h3>
-          <p>Dale al boton para iniciar sesión</p>
-          <button id="btn__iniciar-sesión"> Iniciar Sesión</button>
+      <main className="contenedor__todo">
+        <section class="caja__trasera">
+          <section class="caja__trasera-login">
+            <h3>¿Eres un administrador?</h3>
+            <p>Ingrese el pin y dale al boton para iniciar sesión</p>
+            <input className="InputValidacion"
+              type="text"
+              placeholder="Ingrese el pin"
+              onChange={(e) => setInputPin(e.target.value)}
+            />
+            <Link
+              to={PinValidacion ? { pathname: "/" } : "/paginaadministrador"}
+              onClick={PinValidacion}
+            >
+              {" "}
+              Administracion
+            </Link>
+          </section>
+          <section class="caja__trasera-register">
+            <h3>¿Eres un cliente?</h3>
+            <p>Dale al boton para entrar a la pagina</p>
+            <Link to={"/cliente"}> Cliente</Link>
+          </section>
         </section>
-        <section class="caja__trasera-register">
-          <h3>¿Eres un cliente?</h3>
-          <p>Dale al boton para entrar a la pagina</p>
-          <Link  to={"/cliente"}> Cliente</Link>
-        </section>
+      </main>
       </section>
-    </section>
-    </main>
-  );
+      )
 };
 
 export default Home;
