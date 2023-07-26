b1bcd62338e0adabd558dfb210d75ceebf51d400
207 <207@academicos.cesde.edu.co>
2023-07-26 15:39:41 -0500

    Agregando estilo al componente total

--- src/components/layouts/clientes/TotalCompras.jsx
+++ src/components/layouts/clientes/TotalCompras.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { database } from "./config/Tareaconnection";
+import HeaderCliente from "../../helpers/HeaderCliente";
 
 
 const TotalCompras = () => {
-    
+    const [listaTareas, setListaTareas] = useState([]);
+    const mostrarTareas = async () => {
+    const tareasCollection = collection(database, "Total-Tienda");
+    const data = await getDocs(tareasCollection);
+    setListaTareas(data.docs.map((doc) => ({ ...doc.data() })));
+    console.log(data.docs.map((doc) => ({ ...doc.data() })));
+  };
+  useEffect(() => {
+    mostrarTareas();
+  }, []);
   return (
-    <>
-    <h1>Total compras</h1>
-        
-    </>
+    <section>
+    <HeaderCliente/>
+    <h3 className="titutarea">Total compras</h3> 
+    <section className="table-container">
+        <table className="creative-table">
+          <thead>
+            <tr>
+              <th>Cantidad</th>
+              <th>Producto</th>
+              <th>Precio</th>
+              <th>Imagen</th>
+            </tr>
+          </thead>
+    <tbody>
+    {listaTareas.map((tareaItem) => (
+     <tr className="uno"  key={tareaItem.nombre}>
+    <td>{tareaItem.cantidad}</td>
+    <td>{tareaItem.producto}</td>
+    <td>{tareaItem.precio}</td>
+    <td><img src={tareaItem.imagen} alt="imagen"/></td>
+    </tr>
+    ))
+    }
+    </tbody>
+    </table>
+    </section>
+    </section>
   )
 }
 
